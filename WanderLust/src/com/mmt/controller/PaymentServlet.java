00dd06ece5aad8e263a4d2258cb86350879eb188
rban23 <rban23@delbahamas46541.sapient.com>
2017-02-28 13:09:06 +0530

    Logic PaymentController

--- WanderLust/src/com/mmt/controller/PaymentServlet.java
+++ WanderLust/src/com/mmt/controller/PaymentServlet.java
@@ -12,20 +12,24 @@ import javax.servlet.http.HttpSession;
 import com.mmt.model.bean.Flight;
 import com.mmt.model.bl.FlightBookingBlMMT;
 import com.mmt.model.bl.FlightPaymentBl;
+import com.mmt.model.bl.PromotionBlMMT;
 
 public class PaymentServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		HttpSession session=request.getSession();
-		String promoPickedID=request.getParameter("promoflight");
-		String flightIDPicked=(String) session.getAttribute("flightId");
-		int noOfSeats=Integer.parseInt((String) session.getAttribute("seats"));
+		HttpSession session = request.getSession();
+		String promoPickedID = request.getParameter("promoflight");
+		String flightIDPicked = (String) session.getAttribute("flightId");
+		String userId=(String) session.getAttribute("userID");
+		int noOfSeats = Integer.parseInt((String) session.getAttribute("seats"));
 		FlightBookingBlMMT flightBookingBlMMT = new FlightBookingBlMMT();
-		FlightPaymentBl flightPaymentBl=new FlightPaymentBl();
+		FlightPaymentBl flightPaymentBl = new FlightPaymentBl();
+		float cartValue = 0;
 		try {
-			float cartValue=flightPaymentBl.cartValue((float) (flightBookingBlMMT.searchFlight(flightIDPicked)).getFlightTicketPrice(), noOfSeats);
+			cartValue = flightPaymentBl.cartValue(
+					(float) (flightBookingBlMMT.searchFlight(flightIDPicked)).getFlightTicketPrice(), noOfSeats);
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -33,6 +37,18 @@ public class PaymentServlet extends HttpServlet {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+		PromotionBlMMT promotionBlMMT = new PromotionBlMMT();
+		try {
+			float valueAfterPromotion=promotionBlMMT.applyPromotion(promotionBlMMT.searchPromotion(promoPickedID), userId, cartValue);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 		
 	}
