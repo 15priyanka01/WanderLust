d7740821d5d5c88a37805b2365147472d0c9276f
rban23 <rban23@delbahamas46541.sapient.com>
2017-02-28 15:10:24 +0530

    FlightBookingJSP Done without CSS

--- WanderLust/src/com/mmt/controller/ConfirmFlightBooking.java
+++ WanderLust/src/com/mmt/controller/ConfirmFlightBooking.java
@@ -1,6 +1,9 @@
 package com.mmt.controller;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,21 +11,38 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.mmt.model.bean.User;
-
+import com.mmt.model.bl.WalletBlMMT;
 
 public class ConfirmFlightBooking extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session=request.getSession();
-		float valueAfterPromotion= (float) session.getAttribute("finalValuetobepaid");
-		User user=(User) session.getAttribute("user");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		float valueAfterPromotion = (float) session.getAttribute("finalValuetobepaid");
+		User user = (User) session.getAttribute("user");
+		boolean paymentStatus = false;
+		WalletBlMMT walletBlMMT = new WalletBlMMT();
+		try {
+			paymentStatus = walletBlMMT.subtractWalletMoney(user.getUserId(), (double) valueAfterPromotion);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+		if(paymentStatus){
+			session.setAttribute("flightBookingStatus", paymentStatus);
+
+			RequestDispatcher dispatch = request.getRequestDispatcher("FinalFlightStep.jsp");
+			dispatch.forward(request, response);
+		}
 	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
